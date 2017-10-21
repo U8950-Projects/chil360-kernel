521fa2cee2ddba7eccc5ec3abdc02bc4298b06e5
alexey podvoyskiy <fell978@yandex.ru>
2017-10-21 21:25:52 +0300

    test patch 2

--- drivers/net/wireless/bcmdhd/wl_cfg80211.c
+++ drivers/net/wireless/bcmdhd/wl_cfg80211.c
@@ -3449,7 +3449,7 @@ wl_cfg80211_set_power_mgmt(struct wiphy *wiphy, struct net_device *dev,
 	/* android has special hooks to change pm when kernel suspended */
 	pm = enabled ? ((dhd->in_suspend) ? PM_MAX : PM_FAST) : PM_OFF;
 #else
-	pm = enabled ? PM_FAST : PM_OFF;
+	pm = PM_OFF; /* enabled ? PM_FAST : PM_OFF; */
 #endif /* SUPPORT_PM2_ONLY */
 
 	if (_net_info->pm_block || wl->vsdb_mode) {
