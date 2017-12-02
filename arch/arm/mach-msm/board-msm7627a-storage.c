be59b7fcf504eb154cac25d1eb624588d17dc33a
alexey podvoyskiy <fell978@yandex.ru>
2017-12-02 13:14:53 +0300

    Update board-msm7627a-storage.c

--- arch/arm/mach-msm/board-msm7627a-storage.c
+++ arch/arm/mach-msm/board-msm7627a-storage.c
@@ -437,16 +437,23 @@ static struct mmc_platform_data sdc4_plat_data = {
 #ifdef CONFIG_HUAWEI_WIFI_SDCC
 #define TAG_BCM			"BCM_4330"
 
-#define PREALLOC_WLAN_NUMBER_OF_SECTIONS	4
-#define PREALLOC_WLAN_NUMBER_OF_BUFFERS		160
-#define PREALLOC_WLAN_SECTION_HEADER		24
+#define WLAN_STATIC_SCAN_BUF0		5
+#define WLAN_STATIC_SCAN_BUF1		6
+#define PREALLOC_WLAN_SEC_NUM		4
+#define PREALLOC_WLAN_BUF_NUM		160
+#define PREALLOC_WLAN_SECTION_HEADER	24
 
-#define WLAN_SECTION_SIZE_0	(PREALLOC_WLAN_NUMBER_OF_BUFFERS * 128)
-#define WLAN_SECTION_SIZE_1	(PREALLOC_WLAN_NUMBER_OF_BUFFERS * 128)
-#define WLAN_SECTION_SIZE_2	(PREALLOC_WLAN_NUMBER_OF_BUFFERS * 512)
-#define WLAN_SECTION_SIZE_3	(PREALLOC_WLAN_NUMBER_OF_BUFFERS * 1024)
+#define WLAN_SECTION_SIZE_0	(PREALLOC_WLAN_BUF_NUM * 128)
+#define WLAN_SECTION_SIZE_1	(PREALLOC_WLAN_BUF_NUM * 128)
+#define WLAN_SECTION_SIZE_2	(PREALLOC_WLAN_BUF_NUM * 512)
+#define WLAN_SECTION_SIZE_3	(PREALLOC_WLAN_BUF_NUM * 1024)
 
-#define WLAN_SKB_BUF_NUM			16
+#define DHD_SKB_HDRSIZE		336
+#define DHD_SKB_1PAGE_BUFSIZE	((PAGE_SIZE*1)-DHD_SKB_HDRSIZE)
+#define DHD_SKB_2PAGE_BUFSIZE	((PAGE_SIZE*2)-DHD_SKB_HDRSIZE)
+#define DHD_SKB_4PAGE_BUFSIZE	((PAGE_SIZE*4)-DHD_SKB_HDRSIZE)
+
+#define WLAN_SKB_BUF_NUM	17
 
 /* for wifi awake */
 #define WLAN_WAKES_MSM        		48	
@@ -467,13 +474,15 @@ static unsigned wifi_config_init[] = {
 };
 
 static struct sk_buff *wlan_static_skb[WLAN_SKB_BUF_NUM];
+static void *wlan_static_scan_buf0;
+static void *wlan_static_scan_buf1;
 
-typedef struct wifi_mem_prealloc_struct {
+struct wlan_mem_prealloc {
 	void *mem_ptr;
 	unsigned long size;
-} wifi_mem_prealloc_t;
+};
 
-static wifi_mem_prealloc_t wifi_mem_array[PREALLOC_WLAN_NUMBER_OF_SECTIONS] = {
+static struct wlan_mem_prealloc wlan_mem_array[PREALLOC_WLAN_SEC_NUM] = {
 	{ NULL, (WLAN_SECTION_SIZE_0 + PREALLOC_WLAN_SECTION_HEADER) },
 	{ NULL, (WLAN_SECTION_SIZE_1 + PREALLOC_WLAN_SECTION_HEADER) },
 	{ NULL, (WLAN_SECTION_SIZE_2 + PREALLOC_WLAN_SECTION_HEADER) },
@@ -483,13 +492,19 @@ static wifi_mem_prealloc_t wifi_mem_array[PREALLOC_WLAN_NUMBER_OF_SECTIONS] = {
 /*wlan static memory alloc*/
 static void *bcm_wifi_mem_prealloc(int section, unsigned long size)
 {
-	if (section == PREALLOC_WLAN_NUMBER_OF_SECTIONS)
+	if (section == PREALLOC_WLAN_SEC_NUM)
 		return wlan_static_skb;
-	if ((section < 0) || (section > PREALLOC_WLAN_NUMBER_OF_SECTIONS))
+	if (section == WLAN_STATIC_SCAN_BUF0)
+		return wlan_static_scan_buf0;
+	if (section == WLAN_STATIC_SCAN_BUF1)
+		return wlan_static_scan_buf1;
+	if ((section < 0) || (section > PREALLOC_WLAN_SEC_NUM))
 		return NULL;
-	if (wifi_mem_array[section].size < size)
+
+	if (wlan_mem_array[section].size < size)
 		return NULL;
-	return wifi_mem_array[section].mem_ptr;
+
+	return wlan_mem_array[section].mem_ptr;
 }
 
 /*wlan power control*/
@@ -525,8 +540,9 @@ static int bcm_wifi_set_power(int enable)
 
 int __init bcm_wifi_init_gpio_mem(void)
 {
-	int i = 0;
+	int i ;
 	int rc = 0;
+	int j ;
 
 	/* config gpio WLAN_WAKES_MSM */
 	rc = gpio_tlmm_config(wlan_wakes_msm[0], GPIO_CFG_ENABLE);	
@@ -568,26 +584,238 @@ int __init bcm_wifi_init_gpio_mem(void)
 
     mdelay(5);
        
-	for(i=0;( i < WLAN_SKB_BUF_NUM );i++) {
-		if (i < (WLAN_SKB_BUF_NUM/2))
-			wlan_static_skb[i] = dev_alloc_skb(4096); 	/* malloc skb 4k buffer */
-		else
-			wlan_static_skb[i] = dev_alloc_skb(32768); 	/* malloc skb 32k buffer */
+	for (i = 0; i < 8; i++) {
+		wlan_static_skb[i] = dev_alloc_skb(DHD_SKB_1PAGE_BUFSIZE);
+		if (!wlan_static_skb[i])
+			goto err_skb_alloc;
 	}
-	for(i=0;( i < PREALLOC_WLAN_NUMBER_OF_SECTIONS );i++) {
-		wifi_mem_array[i].mem_ptr = kmalloc(wifi_mem_array[i].size,
-							GFP_KERNEL);
-		if (wifi_mem_array[i].mem_ptr == NULL)
-			return -ENOMEM;
+
+	for (; i < 16; i++) {
+		wlan_static_skb[i] = dev_alloc_skb(DHD_SKB_2PAGE_BUFSIZE);
+		if (!wlan_static_skb[i])
+			goto err_skb_alloc;
 	}
-	
-	printk("%s: %s bcm_wifi_init_gpio_mem successfully\n" , __func__ , TAG_BCM );
+
+	wlan_static_skb[i] = dev_alloc_skb(DHD_SKB_4PAGE_BUFSIZE);
+	if (!wlan_static_skb[i])
+		goto err_skb_alloc;
+
+	for (i = 0 ; i < PREALLOC_WLAN_SEC_NUM ; i++) {
+		wlan_mem_array[i].mem_ptr =
+			kmalloc(wlan_mem_array[i].size, GFP_KERNEL);
+
+		if (!wlan_mem_array[i].mem_ptr)
+			goto err_mem_alloc;
+	}
+	wlan_static_scan_buf0 = kmalloc(65536, GFP_KERNEL);
+	if (!wlan_static_scan_buf0)
+		goto err_mem_alloc;
+	wlan_static_scan_buf1 = kmalloc(65536, GFP_KERNEL);
+	if (!wlan_static_scan_buf1)
+		goto err_mem_alloc;
+
 	return 0;
+
+ err_mem_alloc:
+	pr_err("Failed to mem_alloc for wifi\n");
+	for (j = 0 ; j < i ; j++)
+		kfree(wlan_mem_array[j].mem_ptr);
+
+	i = WLAN_SKB_BUF_NUM;
+
+ err_skb_alloc:
+	pr_err("Failed to skb_alloc for wifi\n");
+	for (j = 0 ; j < i ; j++)
+		dev_kfree_skb(wlan_static_skb[j]);
+
+	return -ENOMEM;
+}
+
+/* Customized Locale table : OPTIONAL feature */
+#define COUNTRY_BUF_SZ	4
+struct cntry_locales_custom {
+	char iso_abbrev[COUNTRY_BUF_SZ];
+	char custom_locale[COUNTRY_BUF_SZ];
+	int  custom_locale_rev;
+};
+
+static struct cntry_locales_custom bcm_wifi_translate_custom_table[] = {
+/* Table should be filled out based on custom platform regulatory requirement */
+	{"",   "XV", 17},	/* Universal if Country code is unknown or empty */
+	{"IR", "XV", 17},	/* Universal if Country code is IRAN, (ISLAMIC REPUBLIC OF) */
+	{"SD", "XV", 17},	/* Universal if Country code is SUDAN */
+	{"SY", "XV", 17},	/* Universal if Country code is SYRIAN ARAB REPUBLIC */
+	{"GL", "XV", 17},	/* Universal if Country code is GREENLAND */
+	{"PS", "XV", 17},	/* Universal if Country code is PALESTINE */
+	{"TL", "XV", 17},	/* Universal if Country code is TIMOR-LESTE (EAST TIMOR) */
+	{"MH", "XV", 17},	/* Universal if Country code is MARSHALL ISLANDS */
+	{"PK", "XV", 17},	/* Universal if Country code is PAKISTAN */
+	{"CK", "XV", 17},	/* Universal if Country code is Cook Island (13.4.27)*/
+	{"CU", "XV", 17},	/* Universal if Country code is Cuba (13.4.27)*/
+	{"FK", "XV", 17},	/* Universal if Country code is Falkland Island (13.4.27)*/
+	{"FO", "XV", 17},	/* Universal if Country code is Faroe Island (13.4.27)*/
+	{"GI", "XV", 17},	/* Universal if Country code is Gibraltar (13.4.27)*/
+	{"IM", "XV", 17},	/* Universal if Country code is Isle of Man (13.4.27)*/
+	{"CI", "XV", 17},	/* Universal if Country code is Ivory Coast (13.4.27)*/
+	{"JE", "XV", 17},	/* Universal if Country code is Jersey (13.4.27)*/
+	{"KP", "XV", 17},	/* Universal if Country code is North Korea (13.4.27)*/
+	{"FM", "XV", 17},	/* Universal if Country code is Micronesia (13.4.27)*/
+	{"MM", "XV", 17},	/* Universal if Country code is Myanmar (13.4.27)*/
+	{"NU", "XV", 17},	/* Universal if Country code is Niue (13.4.27)*/
+	{"NF", "XV", 17},	/* Universal if Country code is Norfolk Island (13.4.27)*/
+	{"PN", "XV", 17},	/* Universal if Country code is Pitcairn Islands (13.4.27)*/
+	{"PM", "XV", 17},	/* Universal if Country code is Saint Pierre and Miquelon (13.4.27)*/
+	{"SS", "XV", 17},	/* Universal if Country code is South_Sudan (13.4.27)*/
+	{"AL", "AL", 2},
+	{"DZ", "DZ", 1},
+	{"AS", "AS", 12},	/* changed 2 -> 12*/
+	{"AI", "AI", 1},
+	{"AG", "AG", 2},
+	{"AR", "AR", 21},
+	{"AW", "AW", 2},
+	{"AU", "AU", 6},
+	{"AT", "AT", 4},
+	{"AZ", "AZ", 2},
+	{"BS", "BS", 2},
+	{"BH", "BH", 4},	/* changed 24 -> 4*/
+	{"BD", "BD", 2},
+	{"BY", "BY", 3},
+	{"BE", "BE", 4},
+	{"BM", "BM", 12},
+	{"BA", "BA", 2},
+	{"BR", "BR", 4},
+	{"VG", "VG", 2},
+	{"BN", "BN", 4},
+	{"BG", "BG", 4},
+	{"KH", "KH", 2},
+	{"CA", "CA", 31},
+	{"KY", "KY", 3},
+	{"CN", "CN", 24},
+	{"CO", "CO", 17},
+	{"CR", "CR", 17},
+	{"HR", "HR", 4},
+	{"CY", "CY", 4},
+	{"CZ", "CZ", 4},
+	{"DK", "DK", 4},
+	{"EE", "EE", 4},
+	{"ET", "ET", 2},
+	{"FI", "FI", 4},
+	{"FR", "FR", 5},
+	{"GF", "GF", 2},
+	{"DE", "DE", 7},
+	{"GR", "GR", 4},
+	{"GD", "GD", 2},
+	{"GP", "GP", 2},
+	{"GU", "GU", 12},
+	{"HK", "HK", 2},
+	{"HU", "HU", 4},
+	{"IS", "IS", 4},
+	{"IN", "IN", 3},
+	{"ID", "KR", 25},	/* ID/1 -> KR/24 */
+	{"IE", "IE", 5},
+	{"IL", "BO", 0},	/* IL/7 -> BO/0 */
+	{"IT", "IT", 4},
+	{"JP", "JP", 58},
+	{"JO", "JO", 3},
+	{"KW", "KW", 5},
+	{"LA", "LA", 2},
+	{"LV", "LV", 4},
+	{"LB", "LB", 5},
+	{"LS", "LS", 2},
+	{"LI", "LI", 4},
+	{"LT", "LT", 4},
+	{"LU", "LU", 3},
+	{"MO", "MO", 2},
+	{"MK", "MK", 2},
+	{"MW", "MW", 1},
+	{"MY", "MY", 3},
+	{"MV", "MV", 3},
+	{"MT", "MT", 4},
+	{"MQ", "MQ", 2},
+	{"MR", "MR", 2},
+	{"MU", "MU", 2},
+	{"YT", "YT", 2},
+	{"MX", "MX", 20},
+	{"MD", "MD", 2},
+	{"MC", "MC", 1},
+	{"ME", "ME", 2},
+	{"MA", "MA", 2},
+	{"NP", "NP", 3},
+	{"NL", "NL", 4},
+	{"AN", "AN", 2},
+	{"NZ", "NZ", 4},
+	{"NO", "NO", 4},
+	{"OM", "OM", 4},
+	{"PA", "PA", 17},
+	{"PG", "PG", 2},
+	{"PY", "PY", 2},
+	{"PE", "PE", 20},
+	{"PH", "PH", 5},
+	{"PL", "PL", 4},
+	{"PT", "PT", 4},
+	{"PR", "PR", 20},
+	{"RE", "RE", 2},
+	{"RO", "RO", 4},
+	{"SN", "SN", 2},
+	{"RS", "RS", 2},
+	{"SG", "SG", 4},
+	{"SK", "SK", 4},
+	{"SI", "SI", 4},
+	{"ES", "ES", 4},
+	{"LK", "LK", 1},
+	{"SE", "SE", 4},
+	{"CH", "CH", 4},
+	{"TW", "TW", 1},
+	{"TH", "TH", 5},
+	{"TT", "TT", 3},
+	{"TR", "TR", 7},
+	{"AE", "AE", 6},
+	{"UG", "UG", 2},
+	{"GB", "GB", 6},
+	{"UY", "UY", 1},
+	{"VI", "VI", 13},
+	{"VA", "VA", 2},
+	{"VE", "VE", 3},
+	{"VN", "VN", 4},
+	{"MA", "MA", 1},
+	{"ZM", "ZM", 2},
+	{"EC", "EC", 21},
+	{"SV", "SV", 19},
+	{"KR", "KR", 57},
+	{"RU", "RU", 13},
+	{"UA", "UA", 8},
+	{"GT", "GT", 1},
+	{"MN", "MN", 1},
+	{"NI", "NI", 2},
+	{"US", "Q2", 57},
+};
+
+static void *bcm_wifi_get_country_code(char *ccode)
+{
+	int size, i;
+	static struct cntry_locales_custom country_code;
+
+	size = ARRAY_SIZE(bcm_wifi_translate_custom_table);
+
+	if ((size == 0) || (ccode == NULL))
+		return NULL;
+
+	for (i = 0; i < size; i++) {
+		if (!strcmp(
+			ccode, bcm_wifi_translate_custom_table[i].iso_abbrev))
+			return &bcm_wifi_translate_custom_table[i];
+	}
+
+	memset(&country_code, 0, sizeof(struct cntry_locales_custom));
+	strlcpy(country_code.custom_locale, ccode, COUNTRY_BUF_SZ);
+
+	return &country_code;
 }
 
 static struct wifi_platform_data bcm_wifi_control = {
 	.mem_prealloc	= bcm_wifi_mem_prealloc,
 	.set_power	=bcm_wifi_set_power,
+	.get_country_code	= bcm_wifi_get_country_code,
 };
 
 static struct platform_device bcm_wifi_device = {
