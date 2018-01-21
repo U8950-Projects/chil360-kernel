52431368cac9fe804e3265f503a54850347f8c07
alexey podvoyskiy <fell978@yandex.ru>
2018-01-21 12:31:41 +0300

    Update dma-contiguous.h

--- arch/arm/include/asm/dma-contiguous.h
+++ arch/arm/include/asm/dma-contiguous.h
@@ -1,24 +1,13 @@
-#ifndef ASM_DMA_CONTIGUOUS_H
-#define ASM_DMA_CONTIGUOUS_H
+#ifndef ASMARM_DMA_CONTIGUOUS_H
+#define ASMARM_DMA_CONTIGUOUS_H
 
 #ifdef __KERNEL__
 #ifdef CONFIG_CMA
 
-#include <linux/device.h>
-#include <linux/dma-contiguous.h>
+#include <linux/types.h>
+#include <asm-generic/dma-contiguous.h>
 
-static inline struct cma *dev_get_cma_area(struct device *dev)
-{
-	if (dev && dev->cma_area)
-		return dev->cma_area;
-	return dma_contiguous_def_area;
-}
-
-static inline void dev_set_cma_area(struct device *dev, struct cma *cma)
-{
-	if (dev)
-		dev->cma_area = cma;
-}
+void dma_contiguous_early_fixup(phys_addr_t base, unsigned long size);
 
 #endif
 #endif
